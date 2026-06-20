ecc905cadb398b7b9023ff4a57aab3a72c15d05b
Sebastian Olszewski <mrsebcio019@gmail.com>
2026-06-20 10:00:30 +0200

    asd
    sda

---
+++ src/test/java/jaz_s32706_nbp/pjatk/PjatkApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz_s32706_nbp.pjatk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjatkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
